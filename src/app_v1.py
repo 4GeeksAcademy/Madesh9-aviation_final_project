59c37f6a87aadccbf7bdea586d3235f5bbdd5c33
Madeshwaran Selvaraj <madeshwaran.selvaraj@outlook.com>
2025-05-09 20:06:19 +0000

    update

--- src/app_v1.py
+++ src/app_v1.py
@@ -3,11 +3,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+import json
 import os
 import plotly.graph_objects as go
 
 # base_dir = os.path.dirname(os.path.dirname(os.path.abspath("app_v1.py")))
-base_dir = os.path.dirname(os.path.abspath("app_v1.py"))
+base_dir = os.path.dirname(os.path.abspath(__file__))
 
 # ---------------------------
 # PAGE CONFIG & BANNER IMAGE
@@ -18,6 +19,58 @@ st.set_page_config(
     page_icon="✈️",
     layout="wide"
 )
+
+# ---------------------------
+# LOAD MODEL AND DATA
+# ---------------------------
+# Load the trained model
+model_path = os.path.join(os.path.dirname(base_dir), "models", "model.pkl")
+
+if not os.path.exists(model_path):
+    st.error(f"Model file not found: {model_path}")
+    st.stop()
+
+with open(model_path, "rb") as f:
+    model = pickle.load(f)
+        
+print("Model loaded successfully.")
+
+#load data
+route_frequency_path = os.path.join(base_dir, "static", "route_frequency.json")
+destination_path = os.path.join(base_dir, "static", "unique_destination.json")
+origin_path = os.path.join(base_dir, "static", "unique_origin.json")
+
+with open(origin_path, 'r') as f:
+    unique_origin = json.load(f)
+
+with open(destination_path, 'r') as f:
+    unique_destination = json.load(f)
+
+with open(route_frequency_path, 'r') as f:
+    route_frequency = json.load(f)
+
+# ---------------------------
+# FUNCTIONS
+# ---------------------------
+def check_route(df, route_frequency):
+    '''Checks if route in frequency, returns None if not present.'''
+
+    # create and encode route
+    df['route'] = df['origin'] + '_' + df['destination']
+
+    if df['route'].iloc[0] not in route_frequency:
+        return None
+    else:
+        df['route_encoded'] = df['route'].map(route_frequency)
+        df['route_encoded'].fillna(0, inplace=True)
+        df.drop(columns=['route'], inplace=True)
+
+    return df
+
+# create and encode time-sin and time-cosine
+def hhmm_to_minutes(hhmm):
+    hours, minutes = map(int, hhmm.split(":"))
+    return hours * 60 + minutes  
 # ---------------------------
 # MAIN APP
 # ---------------------------
@@ -137,9 +190,6 @@ def main():
             }
             </style>
             """, unsafe_allow_html=True)
-        # load dataset
-        data_path = os.path.join(base_dir, "data", "processed", "combined_data.csv")
-        data_df = pd.read_csv(data_path)
 
         flight_details = {
             "origin": None,
@@ -172,9 +222,9 @@ def main():
         # create three columns for parallel display
             col1, col2, col3 = st.columns(3)
             with col1: 
-                flight_details["origin"] = st.selectbox(label="Enter the departure airport: ", options=data_df['origin'].unique(), placeholder='LGA')
+                flight_details["origin"] = st.selectbox(label="Enter the departure airport: ", options=unique_origin, placeholder='LGA')
             with col2: 
-                flight_details["destination"] = st.selectbox(label="Enter the destination airport: ", options=data_df['destination'].unique(), placeholder='ORF')
+                flight_details["destination"] = st.selectbox(label="Enter the destination airport: ", options=unique_destination, placeholder='ORF')
             with col3: 
                 flight_details["departure_time"] = st.time_input("Enter your departure time (use military time): ")
             # submit button
@@ -192,35 +242,19 @@ def main():
                     "destination": [flight_details["destination"]],
                     "departure_time": [flight_details["departure_time"].strftime("%H:%M")]
                 })
-                # create and encode route
-                route_frequency = data_df['origin'] + '_' + data_df['destination']
-                route_frequency = route_frequency.value_counts().to_dict()
-                df['route'] = df['origin'] + '_' + df['destination']
-                df['route_encoded'] = df['route'].map(route_frequency)
-                df['route_encoded'].fillna(0, inplace=True)
-                df.drop(columns=['route'], inplace=True)
-                print(df)
+                # Check if the route is valid
+                df=check_route(df, route_frequency)
 
-                # create and encode time-sin and time-cosine
-                def hhmm_to_minutes(hhmm):
-                    hours, minutes = map(int, hhmm.split(":"))
-                    return hours * 60 + minutes  
+            if df is None:
+                st.error(f"Invalid Flight Route. Please check your origin and destination.")
+
+            else:
         
                 df['Time'] = df['departure_time'].apply(hhmm_to_minutes)
                 df['time_sin'] = np.sin(2 * np.pi * df['Time'] / 1440)  # 1440 minutes in a day
                 df['time_cos'] = np.cos(2 * np.pi * df['Time'] / 1440)
         
                 df = df[['time_sin', 'time_cos', 'route_encoded']]
-        
-                # Load the trained model
-                model_path = os.path.join(base_dir, "models", "model.pkl")
-        
-                if not os.path.exists(model_path):
-                    st.error(f"Model file not found: {model_path}")
-                    st.stop()
-
-                with open(model_path, "rb") as f:
-                    model = pickle.load(f)
         
                 # make the predictions
                 probability = model.predict_proba(df)
@@ -257,6 +291,8 @@ def main():
     with tab2:
         st.header("Model Performance Metrics")
         st.info("Model evaluation metrics and visualizations will appear here.")
+        st.image(os.path.join(os.path.dirname(__file__), "static", "crash_1.gif"), use_container_width=True)
+    st.divider()
 
     # TAB 3: Data Exploration UI Skeleton
     with tab3:
